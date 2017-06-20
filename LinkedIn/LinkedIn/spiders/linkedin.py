3d78d91c4a3de0c982e42d8072a2bbf54086b26e
Gihihi <maqianli9348@icloud.com>
2017-06-20 09:06:02 -0700

    解析个人主页

--- LinkedIn/LinkedIn/spiders/linkedin.py
+++ LinkedIn/LinkedIn/spiders/linkedin.py
@@ -23,7 +23,7 @@ WHITE_LIST = [
 
 KEY_WORDS = [
 		'小米',
-		'阿里巴巴',
+		#'阿里巴巴',
 	]
 
 
@@ -51,36 +51,43 @@ class LinkedinSpider(scrapy.Spider):
             }
 
 	def parse(self, response):
+		'''
+			根据关键字进行搜索
+		'''
 		for key_word in KEY_WORDS:
-			url = self.start_url + key_word #+ self.end_url + '1'
-			#yield scrapy.FormRequest(url, cookies=self.cookies, callback=self.parse_page, meta={'key_word' : key_word})
+			url = self.start_url + key_word
 			yield scrapy.Request(url, cookies=self.cookies, callback=self.parse_page, meta={'key_word' : key_word})
 
 	def parse_page(self, response):
-		for i in range(1, 4):
+		'''
+			翻页处理
+		'''
+		for i in range(1, 2):
 			url = self.start_url + response.meta['key_word'] + self.end_url + str(i)
-			#yield scrapy.FormRequest(url, cookies=self.cookies, callback=self.parse_person_url)
 			yield scrapy.Request(url, cookies=self.cookies, callback=self.parse_person_url)
 
 	def parse_person_url(self, response):
+		'''
+			解析非好友个人主页url
+		'''
 		pattern = re.compile('.*?&quot;publicIdentifier&quot;:&quot;(.*?)&quot;.*?', re.S)
 		items = re.findall(pattern, response.body)
-		url_list = []
-		print ('==========================================================')
 		for item in items:
 			if item not in WHITE_LIST: 
-				url_list.append(self.person_url +  item)
-		print(url_list)
-		print ('==========================================================')
-        
-        
-        #with open('result.html', 'w') as filename:
-        #    filename.write(response.body)
-        #print ('==========================================================')
-        #print response.url
-        #print ('==========================================================')
-        #name_list = response.xpath('//span[@class="name actor-name"]/text()').extract()
-        #a = response.xpath('//*[@id="ember3406"]/span[1]').extract()
-        #print('================================')
-        #print(a)
-       
+				url = self.person_url +  item
+				yield scrapy.Request(url, cookies=self.cookies, callback=self.parse_person_info)
+	
+	def parse_person_info(self, response):
+		'''
+			解析LinkedIn个人主页
+		'''
+		content = response.body.replace('&quot;', '"')
+		#print ('====================================')
+		#print (response.url)
+		#print ('====================================')
+		profile_txt = re.findall('', content)
+		firstname = re.findall('"firstname":"(.*?)"', profile_txt)
+		lastname = re.findall('"lastname":"(.*?)"', profile_txt)
+
+		if firstname and lastname:
+			print '姓名: %s%s LinkedIn: %s' %(lastname[0], firstname[0], response.url)
