8046ed4d394bec8d6d7f113a329c580b267d4501
Gihihi <maqianli9348@icloud.com>
2017-07-20 07:39:34 -0700

    url

--- LinkedIn/LinkedIn/spiders/get_url.py
+++ LinkedIn/LinkedIn/spiders/get_url.py
@@ -13,20 +13,19 @@ class GetUrlSpider(scrapy.Spider):
 	end_url = '"]&facetGeoRegion=["cn:0"]&page='
 	
 	home_path ='/home/python/GitHub/LinkedInSpider/url_info/url/'
-
+	
 	def start_requests(self):
 		'''
 			搜索关键字
 		'''
 		for company_id in KEY_WORDS.keys():
-			for page in range(1,6):
+			for page in range(1,3):
 				url = self.start_url + company_id + self.end_url + str(page)
 				yield scrapy.Request(url, cookies=cookie, callback=self.parse, meta={'cid' : company_id, 'page' : str(page)})
 
 	def parse(self, response):
 		
 		content = response.body
-		company = KEY_WORDS[response.meta['cid']]
 
-		with open (self.home_path + company + '/'  + response.meta['cid'] + '_' + response.meta['page'] + '.txt', 'w') as f:
+		with open (self.home_path + response.meta['cid'] + '_' + response.meta['page'] + '.txt', 'w') as f:
 			f.write(content)
